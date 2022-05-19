31026d5bd3c948b83e65e24acd2e4dd13bd1b3aa
kazuya1029 <kazubs@icloud.com>
2022-05-19 22:18:37 +0900

    1st commit

---
+++ main.py
@@ -0,0 +1,46 @@
+import streamlit as st
+import time
+
+st.title('Streamlit 超入門')
+
+st.write("プログレスバーの表示")
+"Start!!"
+
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+    latest_iteration.text(f"Iteration{i+1}")
+    bar.progress(i+1)
+    time.sleep(0.1)
+
+"Done!!"
+
+
+
+
+
+
+
+# left_column,right_column = st.beta_columns(2)
+# button = left_column.button("右カラムに文字を表示")
+# if button:
+#     right_column.write("ここは右コラムです")
+
+# expander1 = st.beta_expander("問い合わせ")
+# expander1.write("問い合わせ内容を書く")
+# expander2 = st.beta_expander("問い合わせ")
+# expander2.write("問い合わせ内容を書く")
+# expander3 = st.beta_expander("問い合わせ")
+# expander3.write("問い合わせ内容を書く")
+
+
+
+# text = st.text_input("あなたの趣味を教えてください。")
+# condition = st.slider("あなたの今の調子は？",0,100,50)
+# "コンディション:",condition
+# "あなたの趣味：",text,"です"
+
+# if st.checkbox("show Image"):
+#     img = Image.open("sample.jpg")
+#     st.image(img, caption="株",use_column_width=True)
